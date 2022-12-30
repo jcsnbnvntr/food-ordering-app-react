90d27c9034be963f8ee3f93d8c105fed9a429fb3
Jayson San Buenaventura <94850885+jcsnbnvntr22@users.noreply.github.com>
2022-12-30 11:43:57 +0800

    refactor: redesign checkout form using grid

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
@@ -6,36 +7,47 @@ const Checkout = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler} className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input id="street" type="text" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input id="postal" type="text" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input id="city" type="text" />
-      </div>
-      <div className={classes.actions}>
-        <button
-          type="button"
-          onClick={props.onCancel}
-          className={classes["button--alt"]}
-        >
-          Cancel
-        </button>
-        <button type="submit" className={classes.submit}>
-          Checkout
-        </button>
-      </div>
-    </form>
+    <Fragment>
+      <form onSubmit={submitHandler} className={classes.form}>
+        <h2>Checkout</h2>
+        <div className={classes.control}>
+          <label htmlFor="firstName">First Name</label>
+          <input id="firstName" type="text" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="name">Last Name</label>
+          <input id="lastName" type="text" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input id="phoneNumber" type="text" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input id="address" type="text" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="postal">Postal Code</label>
+          <input id="postal" type="text" />
+        </div>
+        <div className={classes.actions}>
+          <button
+            type="button"
+            onClick={props.onCancel}
+            className={classes["button--alt"]}
+          >
+            Cancel
+          </button>
+          <button type="submit" className={classes.submit}>
+            Confirm
+          </button>
+        </div>
+      </form>
+    </Fragment>
   );
 };
 
